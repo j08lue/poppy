11b6779a922a07fec353fb1dd39eada6ec124ced
j08lue <j08lue@gmail.com>
2014-03-31 11:34:52 +0200

    added function barotropic_stream_function_section

--- poppy/stream_functions.py
+++ poppy/stream_functions.py
@@ -57,7 +57,7 @@ def get_vertical_stream_function(ds,region='Global',t=0):
 
 
 
-def get_barotropic_stream_function(ds,region,lon0=None,t=0):
+def get_barotropic_stream_function(ds,region=None,lon0=None,t=0):
     """Get barotropic stream function for a given region
     
     Parameters
@@ -80,24 +80,83 @@ def get_barotropic_stream_function(ds,region,lon0=None,t=0):
     lon = dsvar['ULONG'][:]
     lat = dsvar['ULAT'][:]
 
-    regmask = poppy.grid.get_regmasks(dsvar['REGION_MASK'][:],int)[region]
+    if region is None:
+        regmask = np.ones(lon.shape)
+    else:
+        regmask = poppy.grid.get_regmasks(dsvar['REGION_MASK'][:],int)[region]
 
-    U = np.zeros(lat.shape)
+    V = np.zeros(lat.shape)
     for k in xrange(len(dsvar['dz'])):
-        U += (_fill0(dsvar['UVEL'][t,k]) * 1e-2
+        V += (_fill0(dsvar['VVEL'][t,k]) * 1e-2
                 * dsvar['dz'][k] * 1e-2)
-    U *= dsvar['DYU'][:,:] * 1e-2
-    U *= regmask
+    V *= dsvar['DXU'][:,:] * 1e-2
+    V *= regmask
 
     if lon0 is not None:
         ix = poppy.grid.ix_rewrap_lon(lon,lon0,lat,latlim=(-60,60))
         lon = lon[ix]
         lat = lat[ix]
-        U = U[ix]
+        V = V[ix]
         regmask = regmask[ix]
 
+    # compute stream function:
+    # cumulative zonal integral of meridional velocity
+    psi = np.cumsum(V[:,::-1],axis=1)[:,::-1]
+    psi *= -1.
+    psi *= 1e-6 # convert to Sv
+    psimasked = np.ma.masked_where(regmask==0,psi)
+
+    return psimasked,lon,lat
+
+
+def get_barotropic_stream_function_section(ds,ii,jj,region=None,t=0):
+    """Get barotropic stream function section
+    
+    Parameters
+    ----------
+    ds : netCDF4.Dataset
+        open netCDF dataset
+    region : str
+        region ID to be used with ``poppy.grid.get_regmasks``
+    lon0 : float
+        longitude at which to start the integration
+    t : int
+        time level (default: 0)
+
+    Returns
+    -------
+    psi,lon,lat
+    """
+    t = t or 0
+    dsvar = ds.variables
+
+    lon = dsvar['ULONG'][jj,ii]
+    lat = dsvar['ULAT'][jj,ii]
+
+    if region is None:
+        regmask = np.ones(lon.shape)
+    else:
+        regmask = poppy.grid.get_regmasks(dsvar['REGION_MASK'][jj,ii],int)[region]
+
+    if isinstance(jj,int):
+        V = np.sum((_fill0(dsvar['VVEL'][t,:,jj,ii]) * 1e-2
+            * dsvar['dz'][:][:,np.newaxis] * 1e-2),axis=0)
+        V *= dsvar['DXU'][jj,ii] * 1e-2
+        V *= regmask
+        psi = np.cumsum(V[::-1])[::-1]
+        psi *= -1.
+
+    elif isinstance(ii,int):
+        U = np.sum((_fill0(dsvar['UVEL'][t,:,jj,ii]) * 1e-2
+            * dsvar['dz'][:][:,np.newaxis] * 1e-2),axis=0)
+        U *= dsvar['DYU'][jj,ii] * 1e-2
+        U *= regmask
+        psi = np.cumsum(U[::-1])[::-1]
+
+    else:
+        raise ValueError('Either ii or jj must be a single integer for integration along a grid line.')
+
     # compute stream function
-    psi = np.cumsum(U[::-1],axis=0)[::-1]
     psi *= 1e-6 # convert to Sv
     psimasked = np.ma.masked_where(regmask==0,psi)
 
