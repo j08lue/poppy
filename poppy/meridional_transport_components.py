d36f15f9b2b18be39c1be9bf56ed802df786c656
j08lue <j08lue@gmail.com>
2014-04-08 08:28:55 +0200

    refinements

--- poppy/meridional_transport_components.py
+++ poppy/meridional_transport_components.py
@@ -1,6 +1,8 @@
 import numpy as np
 from oceanpy.stats import central_differences
 
+def _fill0(a):
+    return np.ma.filled(a,0.)
 
 def mean_velocity_component(ds,varn,regmask=1,kza=0,kzo=None,S0=34.8):
     """Mean velocity component using VNT or VNS
@@ -13,21 +15,22 @@ def mean_velocity_component(ds,varn,regmask=1,kza=0,kzo=None,S0=34.8):
     
     This means that UET has to be multiplied by DXU (and VNT by DYU) to get units [degC m s-1]
     """
-    dxu = ds.variables['DXU'][:]/100.
-    dyu = ds.variables['DYU'][:]/100.
-    dz = ds.variables['dz'][:]/100.
+    dsvar = ds.variables
+    dxu = dsvar['DXU'][:]/100.
+    dyu = dsvar['DYU'][:]/100.
+    dz = dsvar['dz'][:]/100.
     if kzo is None: kzo = len(dz)
     meanvel = np.zeros(regmask.shape[0])
     for k in xrange(kza,kzo):
         if varn == 'heat':
-            layer = ds.variables['VNT'][0,k] # degC s-1
+            layer = dsvar['VNT'][0,k] # degC s-1
             layer *= dyu
         elif varn == 'salt':
-            layer = ds.variables['VNS'][0,k] # PPT s-1
+            layer = dsvar['VNS'][0,k] # PPT s-1
             layer *= dyu
         elif varn == 'freshwater':
-            layer = (S0 - ds.variables['SALT'][0,k]) / S0
-            layer *= ds.variables['VVEL'][0,k]/100.
+            layer = (S0 - dsvar['SALT'][0,k]) / S0
+            layer *= dsvar['VVEL'][0,k]/100.
         layer *= dz[k]
         layer *= dxu
         layer *= regmask
@@ -41,19 +44,20 @@ def mean_velocity_component(ds,varn,regmask=1,kza=0,kzo=None,S0=34.8):
 
 def diffusion_component(ds,varn,regmask=1,kza=0,kzo=None,S0=34.8):
     """Temperature/Salt diffusion"""
-    dxt = ds.variables['DXT'][:]/100.
-    dyt = ds.variables['DYT'][:]/100.
-    dz = ds.variables['dz'][:]/100.
+    dsvar = ds.variables
+    dxt = dsvar['DXT'][:]/100.
+    dyt = dsvar['DYT'][:]/100.
+    dz = dsvar['dz'][:]/100.
     if kzo is None: kzo = len(dz)
     diffusion = np.zeros(regmask.shape[0])
     for k in xrange(kza,kzo):
-        layer = np.ma.filled(ds.variables['KAPPA_ISOP'][0,k]/1e4,0) # m2 s-1
+        layer = _fill0(dsvar['KAPPA_ISOP'][0,k]/1e4) # m2 s-1
         if varn == 'heat':
-            scalar = np.ma.filled(ds.variables['TEMP'][0,k],0)
+            scalar = _fill0(dsvar['TEMP'][0,k])
         elif varn == 'salt':
-            scalar = np.ma.filled(ds.variables['SALT'][0,k],0)
+            scalar = _fill0(dsvar['SALT'][0,k])
         elif varn == 'freshwater':
-            scalar = (S0 - np.ma.filled(ds.variables['SALT'][0,k],0)) / S0
+            scalar = (S0 - _fill0(dsvar['SALT'][0,k])) / S0
         layer *= central_differences(scalar,dyt,axis=0) # [scalar] m s-1
         layer *= dz[k]
         layer *= dxt
@@ -68,16 +72,17 @@ def diffusion_component(ds,varn,regmask=1,kza=0,kzo=None,S0=34.8):
 
 def bolus_velocity_component_vnt_isop(ds,varn,regmask=1,kza=0,kzo=None,S0=0):
     """Eddy-induced velocity / bolus velocity using VNT_ISOP"""
-    dxu = ds.variables['DXU'][:]/100.
-    dyu = ds.variables['DYU'][:]/100.
-    dz = ds.variables['dz'][:]/100.
+    dsvar = ds.variables
+    dxu = dsvar['DXU'][:]/100.
+    dyu = dsvar['DYU'][:]/100.
+    dz = dsvar['dz'][:]/100.
     if kzo is None: kzo = len(dz)
     bolus = np.zeros(regmask.shape[0])
     for k in xrange(kza,kzo):
         if varn == 'heat':
-            layer = ds.variables['VNT_ISOP'][0,k] # degC s-1
+            layer = dsvar['VNT_ISOP'][0,k] # degC s-1
         elif varn == 'salt':
-            layer = ds.variables['VNS_ISOP'][0,k] # PPT s-1
+            layer = dsvar['VNS_ISOP'][0,k] # PPT s-1
         elif varn == 'freshwater':
             raise NotImplementedError('Salinity normalization does not work with this function.\n \
                     Use `_bolus_velocity_component_visop` instead.')
@@ -95,18 +100,19 @@ def bolus_velocity_component_vnt_isop(ds,varn,regmask=1,kza=0,kzo=None,S0=0):
 
 def bolus_velocity_component_visop(ds,varn,regmask=1,kza=0,kzo=None,S0=0):
     """Eddy-induced velocity / bolus velocity using VISOP variable"""
-    dxu = ds.variables['DXU'][:]/100.
-    dz = ds.variables['dz'][:]/100.
+    dsvar = ds.variables
+    dxu = dsvar['DXU'][:]/100.
+    dz = dsvar['dz'][:]/100.
     if kzo is None: kzo = len(dz)
     bolus = np.zeros(regmask.shape[0])
     for k in xrange(kza,kzo):
-        layer = ds.variables['VISOP'][0,k]/100.
+        layer = dsvar['VISOP'][0,k]/100.
         if varn == 'heat':
-            layer *= ds.variables['TEMP'][0,k]
+            layer *= dsvar['TEMP'][0,k]
         elif varn == 'salt':
-            layer *= ds.variables['SALT'][0,k]
+            layer *= dsvar['SALT'][0,k]
         elif varn == 'freshwater':
-            layer = (S0 - np.ma.filled(ds.variables['SALT'][0,k],0)) / S0
+            layer = (S0 - _fill0(dsvar['SALT'][0,k])) / S0
         layer *= dxu
         layer *= dz[k]
         layer *= regmask
