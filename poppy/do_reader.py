35784f681c6ecbff21251d9a5243df3368257231
j08lue <j08lue@gmail.com>
2014-08-27 18:39:24 +0200

    now merging TS and tr data into single df

--- poppy/do_reader.py
+++ poppy/do_reader.py
@@ -50,56 +50,59 @@ def read_do_file(fname,straits=['DS','FBC','RossSea','WeddellSea'],year=None):
         columns in the do file
     """
     nstraits = len(straits)
-
-    ovf_TS = [StringIO() for s in straits]
-    ovf_tr = [StringIO() for s in straits]
-    lines_TS = []
-    lines_tr = []
+    contents = ['TS','tr']
+    
+    ios = {}
+    lines = {}
+    for key in contents:
+        ios[key] = [StringIO() for s in straits]
+        lines[key] = []
 
     # split lines into TS and tr
     with open(fname,'r') as fin:
         for line in fin:
             if line.startswith(' ovf_TS'):
-                lines_TS.append(line[18:])
+                lines['TS'].append(line[18:])
             elif line.startswith(' ovf_tr'):
-                lines_tr.append(line[18:])
+                lines['tr'].append(line[18:])
 
     # separate out straits
-    for i in xrange(nstraits):
-        # take every (nstraits)th line starting from 0,1,2,3...
-        ovf_TS[i].writelines(itertools.islice(lines_TS,i,None,nstraits))
-        ovf_tr[i].writelines(itertools.islice(lines_tr,i,None,nstraits))
-        # rewind StringIOs
-        ovf_TS[i].seek(0)
-        ovf_tr[i].seek(0)
+    for key in contents:
+        for i in xrange(nstraits):
+            # take every (nstraits)th line starting from 0,1,2,3...
+            ios[key][i].writelines(itertools.islice(lines[key],i,None,nstraits))
+            # rewind StringIOs
+            ios[key][i].seek(0)
 
     # get start day
     year = year or _yr_from_fname(fname)
 
-    # Read separate straits into panda dfs
-    dfs_TS = []
-    dfs_tr = []
-    for i,strait in enumerate(straits):
-        # TS
-        df = pd.read_csv(ovf_TS[i],
-                names=['n','Ti','Si','Ts','Ss','Te','Se','Tp','Sp'],
-                delim_whitespace=True)
-        _add_index(df,year)
-        dfs_TS.append(df)
-        # tr
-        df = pd.read_csv(ovf_tr[i],
-                names=['n','phi','Ms','Me','Mp','m','zt'],
-                delim_whitespace=True)
-        _add_index(df,year)
-        dfs_tr.append(df)
+    cols = dict(
+            TS = ['n','Ti','Si','Ts','Ss','Te','Se','Tp','Sp'],
+            tr = ['n','phi','Ms','Me','Mp','m','zt'])
+    usecols = dict(
+            TS = np.arange(1,len(cols['TS'])),
+            tr = np.arange(1,len(cols['tr'])))
 
-    return dict(
-            TS = pd.concat(dfs_TS,keys=straits,names=('Strait',dfs_TS[0].index.names[0])),
-            tr = pd.concat(dfs_tr,keys=straits,names=('Strait',dfs_tr[0].index.names[0])),
-        )
+    dfs = defaultdict(list)
+    for key in contents:
+        # select column names from usecols
+        names = np.array(cols[key])[usecols[key]]
+        # read separate straits into panda dfs
+        for i,strait in enumerate(straits):
+            df = pd.read_csv(ios[key][i],
+                    names=names,
+                    usecols=usecols[key],
+                    delim_whitespace=True)
+            _add_index(df,year)
+            dfs[key].append(df)
+
+    return pd.concat(
+            [pd.concat(dfs[key],keys=straits,names=('Strait','ModelYear')) 
+                for key in contents],axis=1)
 
 
-def read_do_multifile(files,datatypes=['TS','tr'],**kwargs):
+def read_do_multifile(files,**kwargs):
     """Read multiple POP diagnostic overflow output (do) files 
     and concatenate their data.
     
@@ -107,8 +110,6 @@ def read_do_multifile(files,datatypes=['TS','tr'],**kwargs):
     ----------
     files : list of str
         files to read and concatenate
-    datatypes : list of str
-        datatypes to store (['TS','tr'])
     """
     if isinstance(files, basestring):
         files = [files]
@@ -116,16 +117,8 @@ def read_do_multifile(files,datatypes=['TS','tr'],**kwargs):
     if len(files) == 1:
         files = sorted(glob.glob(files[0]))
 
-    dfs = defaultdict(list)
+    dfs = []
     for fname in files:
-        data = read_do_file(fname,**kwargs)
-        for key in datatypes:
-            dfs[key].append(data[key])
-    df = {}
-    for key in datatypes:
-        df[key] = pd.concat(dfs[key])
-        df[key].files = files
-    if len(datatypes) == 1:
-        return df[datatypes[0]]
-    else:
-        return df
+        df = read_do_file(fname,**kwargs)
+        dfs.append(df)
+    return pd.concat(dfs)
