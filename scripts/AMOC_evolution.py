62d67c34212600040af1198ff29cc54bb2d92b1d
j08lue <j08lue@gmail.com>
2015-05-18 13:02:41 +0200

    removed file saving

--- scripts/AMOC_evolution.py
+++ scripts/AMOC_evolution.py
@@ -1,41 +1,23 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt ; plt.close('all')
-import cPickle as pickle
+plt.style.use('ggplot')
 import glob
 
 import poppy.metrics 
-poppy.metrics.use_pandas = False
-
-
-def save_amoc_time_series(pattern, outfname, latlim=(30,60), zlim=(500,9999)):
-    """Plot AMOC time series from CESM/POP data"""
-    ncfiles = sorted(glob.glob(pattern))
-    maxmeanamoc,timeax = poppy.metrics.get_amoc(ncfiles, latlim=latlim, zlim=zlim)
-    dataout = dict(
-            timeax=timeax,
-            maxmeanamoc=maxmeanamoc,
-            latlim=latlim,zlim=zlim)
-    with open(outfname,'wb') as fout:
-        pickle.dump(dataout,fout)
 
 
 def plot_amoc_time_series(pattern, latlim=(30,60), zlim=(500,9999), savefig=False, figname=''):
     """Plot AMOC time series from CESM/POP data"""
     ncfiles = sorted(glob.glob(pattern))
-    maxmeanamoc,timeax = poppy.metrics.get_amoc(ncfiles, latlim=latlim, zlim=zlim)
-    fig = plt.figure()
-    ax = fig.gca()
-    ax.plot(timeax,maxmeanamoc,'b')
-    ax.set_xlim(timeax.min(),timeax.max())
-    ax.grid(True)
-    ax.set_ylabel('AMOC (Sv)')
-    ax.set_xlabel('integration year')
-    ax.xaxis_date()
+    ts = poppy.metrics.get_amoc(ncfiles, latlim=latlim, zlim=zlim)
+    ts.plot()
+    plt.ylabel('AMOC (Sv)')
+    plt.xlabel('integration year')
 
     if savefig or figname:
         figname = figname or 'AMOC_time_series.png'
-        fig.savefig(figname,dpi=300)
+        plt.gca().figure.savefig(figname,dpi=300)
     else:
         plt.show()
 
@@ -61,23 +43,11 @@ if __name__ == "__main__":
             help='set this to save the figure with default filename')
     parser.add_argument('--figname',type=str,
             help='save the figure to the given filename (implies -s)')
-    parser.add_argument('--savetofile',dest='outfname',type=str,
-            help='save the data to this file (pickle)')
     
     args = parser.parse_args()
-    argsdict = vars(args)
 
     if len(args.files) == 1:
-        argsdict['files'] = sorted(glob.glob(args.files[0]))
+        args.files = sorted(glob.glob(args.files[0]))
  
-    if args.outfname is not None:
-        for key in ['savefig','figname']:
-            try:
-                del argsdict[key]
-            except KeyError:
-                pass
-        save_amoc_time_series(**argsdict)
-    else:
-        del argsdict['outfname']
-        plot_amoc_time_series(**argsdict)
+    plot_amoc_time_series(vars(args))
  
