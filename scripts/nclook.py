a71f8171dee7a3761dbefc926c375847f8ae2a21
j08lue <j08lue@gmail.com>
2015-09-07 10:24:08 +0200

    file opening now reusable function

--- scripts/nclook.py
+++ scripts/nclook.py
@@ -21,33 +21,33 @@ except ImportError:
     import netCDF4
     _ncmodule = 'netCDF4'
 
-parser = argparse.ArgumentParser(description="Open one or several netCDF files from command line")
-parser.add_argument('ncfiles', nargs='+', help='path to netCDF file')
-args = parser.parse_args()
 
-try:
-    if _ncmodule == 'xray':
-        # open files with xray
-        try:
-            ds = xray.open_mfdataset(args.ncfiles)
+def open_files(ncfiles, return_dsvar=False):
+    """Open netCDF files, either with xray or netCDF4"""
+    try:
+        if _ncmodule == 'xray':
+            # open files with xray
+            try:
+                ds = xray.open_mfdataset(ncfiles)
+            except ValueError:
+                ds = xray.open_mfdataset(ncfiles, decode_times=False)
+                print('Warning: Using decode_times=False')
             dsvar = ds
-        except ValueError:
-            ds = xray.open_mfdataset(args.ncfiles, decode_times=False)
-            print('Warning: using decode_times=False')
-    else:
-        # open files with netCDF4
-        if len(args.ncfiles) > 1:
-            ds = netCDF4.MFDataset(args.ncfiles)
         else:
-            ds = netCDF4.Dataset(args.ncfiles[0])
-        dsvar = ds.variables
-    print('opened {} netCDF file(s) into namespace as \'ds\' using {}'.format(
-        len(args.ncfiles), _ncmodule))
-except RuntimeError as err:
-    traceback.print_exc(err)
-    print('Warning: File(s) could not be opened: {}'.format(args.ncfiles))
-    dsvar = None
-
+            # open files with netCDF4
+            if len(ncfiles) > 1:
+                ds = netCDF4.MFDataset(ncfiles)
+            else:
+                ds = netCDF4.Dataset(ncfiles[0])
+            dsvar = ds.variables
+    except RuntimeError as err:
+        traceback.print_exc(err)
+        print('Warning: File(s) could not be opened: {}'.format(ncfiles))
+        dsvar = None
+    if return_dsvar:
+        return ds, dsvar
+    else:
+        return ds
 
 
 def save_figure(fname, fig=None):
@@ -61,65 +61,78 @@ def save_figure(fname, fig=None):
         fig.savefig(fname, dpi=200)
 
 
-# Convenience functions for netCDF4
-if _ncmodule == 'netCDF4':
 
-    def quickplot(varn, k, dsvar=dsvar, t=0, cmap='cubehelix', **kwarg):
-        """Make a quick plot of the given variable at levek `k` and time step `t`
-        
-        Parameters
-        ----------
-        varn : str
-            name of 4D variable present in `dsvar`
-        k : int
-            vertical level index
-        t : int
-            time level index
-        cmap : str or colormap
-            color map
-        **kwarg : dict
-            keyword arguments passed to plt.pcolormesh
-        """
-        fig = plt.figure()
-        plt.title('{} at {:.0f} m'.format(varn, dsvar['z_t'][k]*1e-2))
-        plt.pcolormesh(dsvar[varn][t,k], cmap=cmap, **kwarg)
-        plt.colorbar(label='{} ({})'.format(dsvar[varn].long_name, dsvar[varn].units))
-        plt.show()
-        return fig
+if __name__ == "__main__":
 
-    def list_variables(units=True,ndim=None,shapes=True):
-        """List variables in netCDF *dataset* including units if *units* for variables in *ndim* dimensions only, if specified"""
-        def _get_variable_list(ds):
-            for varn in ds.variables.keys():
-                if ndim is not None:
-                    if len(ds.variables[varn].shape) != ndim:
-                        continue
-                s = ''
-                try:
-                    s += '{} :'.format(varn)
-                except:
-                    pass
-                try:
-                    s += ' {}'.format(ds.variables[varn].long_name)
-                except:
-                    pass
-                try:
-                    varunits = ds.variables[varn].units
-                    if units and varunits:
-                        s += ' [{}]'.format(varunits)
-                except:
-                    pass
-                if shapes:
+    parser = argparse.ArgumentParser(description="Open one or several netCDF files from command line")
+    parser.add_argument('ncfiles', nargs='+', help='path to netCDF file')
+    args = parser.parse_args()
+
+    print('opening {} netCDF file(s) into namespace as \'ds\' using {} ...'.format(
+        len(args.ncfiles), _ncmodule))
+    ds, dsvar = open_files(args.ncfiles, return_dsvar=True)
+
+
+    # Convenience functions for netCDF4
+    if _ncmodule == 'netCDF4':
+
+        def quickplot(varn, k, dsvar=dsvar, t=0, cmap='cubehelix', **kwarg):
+            """Make a quick plot of the given variable at levek `k` and time step `t`
+            
+            Parameters
+            ----------
+            varn : str
+                name of 4D variable present in `dsvar`
+            k : int
+                vertical level index
+            t : int
+                time level index
+            cmap : str or colormap
+                color map
+            **kwarg : dict
+                keyword arguments passed to plt.pcolormesh
+            """
+            fig = plt.figure()
+            plt.title('{} at {:.0f} m'.format(varn, dsvar['z_t'][k]*1e-2))
+            plt.pcolormesh(dsvar[varn][t,k], cmap=cmap, **kwarg)
+            plt.colorbar(label='{} ({})'.format(dsvar[varn].long_name, dsvar[varn].units))
+            plt.show()
+            return fig
+
+        def list_variables(units=True,ndim=None,shapes=True):
+            """List variables in netCDF *dataset*
+            including units if *units* for variables 
+            in *ndim* dimensions only, if specified"""
+            def _get_variable_list(ds):
+                for varn in ds.variables.keys():
+                    if ndim is not None:
+                        if len(ds.variables[varn].shape) != ndim:
+                            continue
+                    s = ''
                     try:
-                        s += ' {}'.format(ds.variables[varn].shape)
+                        s += '{} :'.format(varn)
                     except:
                         pass
-                    # print output
-                if s: 
-                    print(s)
-        try:
-            with netCDF4.Dataset(ds) as ds_new:
-                _get_variable_list(ds_new)
-        except:
-            _get_variable_list(ds)
-
+                    try:
+                        s += ' {}'.format(ds.variables[varn].long_name)
+                    except:
+                        pass
+                    try:
+                        varunits = ds.variables[varn].units
+                        if units and varunits:
+                            s += ' [{}]'.format(varunits)
+                    except:
+                        pass
+                    if shapes:
+                        try:
+                            s += ' {}'.format(ds.variables[varn].shape)
+                        except:
+                            pass
+                        # print output
+                    if s: 
+                        print(s)
+            try:
+                with netCDF4.Dataset(ds) as ds_new:
+                    _get_variable_list(ds_new)
+            except:
+                _get_variable_list(ds)
