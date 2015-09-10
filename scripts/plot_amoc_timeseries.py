26b50deefc56324e2837ee96d2dfad3cf69ec568
j08lue <j08lue@gmail.com>
2015-09-10 14:02:26 +0200

    renamed

---
+++ scripts/plot_amoc_timeseries.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt ; plt.close('all')
+plt.style.use('ggplot')
+import glob
+
+import poppy.metrics 
+
+def plot_amoc_time_series(files, latlim=(30,60), zlim=(500,9999), savefig=False, figname=''):
+    """Plot AMOC time series from CESM/POP data"""
+    ts = poppy.metrics.get_amoc(files, latlim=latlim, zlim=zlim)
+    ts.plot()
+    plt.ylabel('AMOC (Sv)')
+    plt.xlabel('integration year')
+    if savefig or figname:
+        figname = figname or 'AMOC_time_series.png'
+        plt.gca().figure.savefig(figname,dpi=300)
+    else:
+        plt.show()
+    return ts
+
+
+if __name__ == "__main__":
+
+    def parse_coords(s):
+        result = map(float,(s.replace('m','-')).split(','))
+        return result[0] if len(result) == 0 else result
+
+    import argparse
+    parser = argparse.ArgumentParser(description="Plot AMOC time series from CESM/POP data")
+    parser.add_argument('files', nargs='+',
+            help='file search pattern. Provide inside "" from command line!',
+            default='*.pop.h.????-??.nc')
+    parser.add_argument('-l','--latlim', type=parse_coords,
+            help='Latitude limits for AMOC region, e.g. 30,60. Accepts negative values as e.g. m30 for -30.', 
+            default=(30,60))
+    parser.add_argument('-z', '--zlim', type=lambda s: map(float,s.split(',')),
+            help='Depth limits for AMOC region, e.g. 500,9999 for below 500 metres.',
+            default=(500,9999))
+    parser.add_argument('-s', dest='savefig', action='store_true',
+            help='set this to save the figure with default filename')
+    parser.add_argument('--figname',type=str,
+            help='save the figure to the given filename (implies -s)')
+
+    args = parser.parse_args()
+
+    if len(args.files) == 1:
+        args.files = sorted(glob.glob(args.files[0]))
+ 
+    ts = plot_amoc_time_series(**vars(args))
+ 
